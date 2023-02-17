b57685166a241b25e7e47e94f8fe9fb05a091f25
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-02-17 15:35:21 +0100

    ✨ Add delete roads save file functionality

--- Assets/Scripts/Authoring/City/Road/SavableRoadTileMapAuthoring.cs
+++ Assets/Scripts/Authoring/City/Road/SavableRoadTileMapAuthoring.cs
@@ -1,16 +1,26 @@
+using System.IO;
 using Unity.Entities;
 using UnityEngine;
 
 public class SavableRoadTileMapAuthoring : MonoBehaviour
 {
 	[SerializeField] private string _saveFilePath;
+	
+	private string ActualSaveFilePath => Application.persistentDataPath + "/" + _saveFilePath;
+
+	[ContextMenu("Delete save file")]
+	private void DeleteSaveFile()
+	{
+		if(File.Exists(ActualSaveFilePath))
+			File.Delete(ActualSaveFilePath);
+	}
 
 	class Baker : Baker<SavableRoadTileMapAuthoring>
 	{
 		public override void Bake(SavableRoadTileMapAuthoring authoring)
 		{
 			var savableTileMap = new SavableRoadTileMap {
-				SaveFilePath = Application.persistentDataPath + "/" + authoring._saveFilePath,
+				SaveFilePath = authoring.ActualSaveFilePath,
 				ShouldLoad = true
 			};
 
