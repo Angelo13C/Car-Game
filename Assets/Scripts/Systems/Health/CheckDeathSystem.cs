ebcf0a33fd773f41fe97f5d2b708825a1a1bb9b4
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-02-17 16:04:11 +0100

    ✨ Add basic health functionality

---
+++ Assets/Scripts/Systems/Health/CheckDeathSystem.cs
@@ -0,0 +1,32 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+[BurstCompile]
+public partial struct CheckDeathSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+    }
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
+        foreach(var (health, entity) in SystemAPI.Query<Health>().WithEntityAccess())
+        {
+            if(health.Current <= 0)
+            {
+                entityCommandBuffer.AddComponent<Dead>(entity);
+            }
+        }
+
+        entityCommandBuffer.Playback(state.EntityManager);
+    }
+}
