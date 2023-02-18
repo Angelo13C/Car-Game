c692733a2c878f9134957a893a0f648ff5b67bff
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-02-18 15:17:36 +0100

    ✨ Add Explosion functionality (also explode on death)

---
+++ Assets/Scripts/Systems/Health/Explosion/ExplodeOnDeathSystem.cs
@@ -0,0 +1,30 @@
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Transforms;
+
+[UpdateAfter(typeof(CheckDeathSystem))]
+[UpdateBefore(typeof(DestroyOnDeathSystem))]
+[BurstCompile]
+public partial struct ExplodeOnDeathSystem : ISystem
+{
+    [BurstCompile]
+    public void OnCreate(ref SystemState state)
+    {
+    }
+    [BurstCompile]
+    public void OnDestroy(ref SystemState state)
+    {
+    }
+
+    [BurstCompile]
+    public void OnUpdate(ref SystemState state)
+    {
+        var entityCommandBuffer = new EntityCommandBuffer(Allocator.Temp);
+        foreach(var (explosive, transform) in SystemAPI.Query<Explosive, TransformAspect>().WithAll<Dead>())
+        {
+            explosive.Explode(entityCommandBuffer, transform.WorldPosition);
+        }
+        entityCommandBuffer.Playback(state.EntityManager);
+    }
+}
