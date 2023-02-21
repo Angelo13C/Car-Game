4b45388d3b7515ac0d42fe11d5fdf02355ceaa47
AngeloCipriani <angelo13cipriani.03@gmail.com>
2023-02-21 12:29:52 +0100

    ⚡️ Burst compile NativeArrayStack

--- Assets/Scripts/Utils/Collections/NativeArrayStack.cs
+++ Assets/Scripts/Utils/Collections/NativeArrayStack.cs
@@ -1,11 +1,14 @@
 using System;
+using Unity.Burst;
 using Unity.Collections;
 
+[BurstCompile]
 public struct NativeArrayStack<T>: IDisposable where T : struct
 {
     private NativeArray<T> _elements;
     public int Length { get; private set; }
 
+    [BurstCompile]
     public bool IsEmpty() => Length == 0;
 
     public NativeArrayStack(int capacity, Allocator allocator)
@@ -14,18 +17,22 @@ public struct NativeArrayStack<T>: IDisposable where T : struct
         Length = 0;
     }
 
+    [BurstCompile]
     public void Push(T element)
     {
         _elements[Length] = element;
         Length++;
     }
+    [BurstCompile]
     public T Pop()
     {
         Length--;
         return _elements[Length];
     }
 
+    [BurstCompile]
     public void Clear() => Length = 0;
 
+    [BurstCompile]
     public void Dispose() => _elements.Dispose();
 }
